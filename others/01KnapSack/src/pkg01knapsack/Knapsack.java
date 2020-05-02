eb4275cd123a1c0afb895060154043dcf9ee4b27
Vincent-hide <hidehiro.okayama@gmail.com>
2020-05-02 00:57:08 -0700

    bottom up approach added

--- others/01KnapSack/src/pkg01knapsack/Knapsack.java
+++ others/01KnapSack/src/pkg01knapsack/Knapsack.java
@@ -1,5 +1,8 @@
 package pkg01knapsack;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 public class Knapsack {
 
     private Item[] items;
@@ -11,15 +14,31 @@ public class Knapsack {
         this.items = items;
         this.capacity = capacity;
         this.numItems = numItems;
-        this.memory = new int[this.numItems][this.capacity+1];
+        this.memory = new int[this.numItems][this.capacity + 1];
     }
 
-    public String printMemoery() {
+    public String printVerticalMemoery() {
         String s = "*** Memoery ***\n";
         for (int memo[] : this.memory) {
             for (int val : memo) {
                 s += "Memoery: " + val + "\n";
             }
+        }
+        return s;
+    }
+
+    public String printMatrixMemory() {
+        String s = "*** Memoery ***\nWeight:\t\t    ";
+        for (int i = 0; i < this.capacity + 1; i++) {
+            s += i + "  ";
+        }
+        s += "\n------------------------------------------------------------\n";
+        for (int i = 0; i < this.numItems; i++) {
+            s += String.format("[V: %d / W: %.2f]: ", this.items[i].value, this.items[i].weight);
+            for (int v = 0; v < this.capacity + 1; v++) {
+                s += this.memory[i][v] < 10 ? " " + this.memory[i][v] + " " : this.memory[i][v] + " ";
+            }
+            s += "\n";
         }
         return s;
     }
@@ -41,8 +60,8 @@ public class Knapsack {
         if (currentWeight <= 0) {
             return 0; // no more capacity
         }
-        if (this.memory[index][(int)currentWeight] != 0) {
-            return this.memory[index][(int)currentWeight]; // if the calculation is done and stored in the memoralization array, return the value
+        if (this.memory[index][(int) currentWeight] != 0) {
+            return this.memory[index][(int) currentWeight]; // if the calculation is done and stored in the memoralization array, return the value
         } else {
             int val1 = 0;
             if (this.items[index].weight <= currentWeight) {
@@ -54,7 +73,7 @@ public class Knapsack {
 //            System.out.println("val2: " + val2);
 //            System.out.println("index: " + index + " - weight: " + currentWeight);
 //            System.out.println("---------------");
-            return this.memory[index][(int)currentWeight] = this.max(val1, val2);
+            return this.memory[index][(int) currentWeight] = this.max(val1, val2);
         }
     }
 
@@ -80,5 +99,99 @@ public class Knapsack {
         int profit2 = this.topDown2(index + 1, currentWeight);
 
         return this.max(profit, profit2);
+    }
+
+    public String bottomUp() {
+        String s = "*** accumulative value ***\n";
+        this.bottomUp(0, 0);
+        s += this.memory[this.numItems - 1][this.capacity] + "\n";
+        s += "*** Items that were chosen ***\n";
+        s += this.takenItemsList();
+        return s;
+    }
+
+    private void bottomUp(int itemIndex, int weightIndex) {
+        if (this.isValidWeight(weightIndex)) {
+            if (weightIndex >= this.items[itemIndex].weight) {
+                if (itemIndex != 0) {
+                    if (this.compareVal(itemIndex, weightIndex)) {
+                        this.memory[itemIndex][weightIndex] = this.items[itemIndex].value;
+                        this.memory[itemIndex][weightIndex] += this.memory[itemIndex - 1][weightIndex - (int) this.items[itemIndex].weight];
+                    } else {
+                        this.memory[itemIndex][weightIndex] = this.memory[itemIndex - 1][weightIndex];
+                    }
+                } else {
+                    this.memory[itemIndex][weightIndex] = this.items[itemIndex].value;
+                }
+            } else {
+                if (itemIndex != 0) {
+                    this.memory[itemIndex][weightIndex] = this.memory[itemIndex - 1][weightIndex];
+                }
+            }
+            this.bottomUp(itemIndex, weightIndex + 1);
+        } else {
+            weightIndex = 0;
+            if (isValidItem(itemIndex + 1)) {
+                this.memory[itemIndex + 1][weightIndex] = 2;
+                this.bottomUp(itemIndex + 1, weightIndex);
+            }
+        }
+    }
+
+    // returns true if the current value is bigger than the value one row above
+    private boolean compareVal(int itemIndex, int weightIndex) {
+        return this.items[itemIndex].value + this.memory[itemIndex - 1][weightIndex - (int) this.items[itemIndex].weight] > this.memory[itemIndex - 1][weightIndex];
+    }
+
+    private boolean isValidWeight(int weightIndex) {
+        return (weightIndex <= this.capacity);
+    }
+
+    private boolean isValidItem(int itemIndex) {
+        return (itemIndex < this.numItems);
+    }
+
+    private String takenItemsList() {
+        String s = "";
+        Stack stack = new Stack();
+        stack = this.takenItemsList(this.numItems - 1, this.capacity, stack);
+        Iterator value = stack.iterator();
+        while (value.hasNext()) {
+            s += this.items[(int) value.next()].id + " & ";
+        }
+        return s;
+    }
+
+    private Stack takenItemsList(int itemIndex, int weightIndex, Stack stack) {
+
+        if (itemIndex < 0 || weightIndex < 0) {
+            return null;
+        }
+
+        // Basecase
+        if (this.memory[itemIndex][weightIndex] == 0) {
+            return stack;
+        }
+
+        if (this.checkIn(itemIndex, weightIndex)) {
+            stack.push(itemIndex);
+            int nextIndex = weightIndex - (int) this.items[itemIndex].weight;
+            this.takenItemsList(itemIndex - 1, nextIndex, stack);
+        } else {
+            this.takenItemsList(itemIndex - 1, weightIndex, stack);
+        }
+
+        return stack;
+    }
+
+    // returns true if the current item was picked, otherwise false
+    private boolean checkIn(int itemIndex, int weightIndex) {
+        if (this.memory[itemIndex][weightIndex] != this.memory[itemIndex - 1][weightIndex]) {
+            int sum = this.items[itemIndex].value + this.memory[itemIndex - 1][weightIndex - (int) this.items[itemIndex].weight];
+            if (sum == this.memory[itemIndex][weightIndex]) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- others/01KnapSack/src/pkg01knapsack/Main.java
+++ others/01KnapSack/src/pkg01knapsack/Main.java
@@ -13,8 +13,17 @@ public class Main {
             new Item(1, 2.00, 2),
             new Item(2, 2.00, 4),
             new Item(3, 4.00, 6),
-            new Item(4, 5.00, 9),};
+            new Item(4, 5.00, 9),
+        };
+        
+        Item[] items3 = {
+            new Item(1, 5.00, 60),
+            new Item(2, 3.00, 50),
+            new Item(3, 4.00, 70),
+            new Item(4, 2.00, 30),
+        };
 
+        
         // w Memorialization -------------------------------------------------
         Knapsack knapsack1 = new Knapsack(items, 3, 50);
         System.out.println("acumulative value: " + knapsack1.topDown());
@@ -33,6 +42,12 @@ public class Main {
         Knapsack knapsack4 = new Knapsack(items2, 4, 8);
         System.out.println("acumulative value: " + knapsack4.topDown2());
         System.out.println("-------------------------------------------------");
+        
+        
+        Knapsack knapsack5 = new Knapsack(items3, 4, 5);
+        System.out.println(knapsack5.bottomUp());
+        System.out.println("-------------------------------------------------");
+        System.out.println(knapsack5.printMatrixMemory());
 
 
     }
